faba881a20133377f752dfd0d6875b96f5ba6964
David Do <davehdo@gmail.com>
2014-09-13 16:38:15 -0400

    moved var to env vars file

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Felldown::Application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'ifelldownhelp.me',
+  :domain => ENV["DOMAIN"],
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
