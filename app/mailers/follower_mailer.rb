8f9f6dc38c367b0c1837113e010b46e87f279a71
eugyev <eugyev@gmail.com>
2014-09-13 14:17:08 -0400

    mailer function takes a follower and a message

--- app/mailers/follower_mailer.rb
+++ app/mailers/follower_mailer.rb
@@ -1,10 +1,11 @@
 class FollowerMailer < ActionMailer::Base
   default from: "from@example.com"
   
-  def send_notification(user)
+  def send_notification(user, message)
       @user = user
+      @message = message #you can adjust the body of the message in the views/follower_mailer/send_notification.erb
       x = mail( :to => @user.email,
-      :subject => 'Thanks for signing up for our amazing app', from: 'ifelldown@help.me' )
+      :subject => message, from: 'ifelldown@help.me' )
     end
     
 end
