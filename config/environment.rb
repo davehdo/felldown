d4f87d4c80616ed7e5fec822ece20a8f5ecf5be5
eugyev <eugyev@gmail.com>
2014-09-13 13:55:20 -0400

    added settings for sendgid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Felldown::Application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'eugyev',
+  :password => 'rolodoc1',
+  :domain => 'ifelldownhelp.me',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
