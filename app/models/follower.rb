192d76b251c6cbe1df06cbd64f74ff5078ac7714
eugyev <eugyev@gmail.com>
2014-09-13 13:55:00 -0400

    added valid email function

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -2,4 +2,8 @@ class Follower < ActiveRecord::Base
   def valid_number?
     self.number.match(/\+\d{11}/) != nil
   end
+  
+  def valid_email?
+    self.email.match(/[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i) ~= nil
+  end
 end
